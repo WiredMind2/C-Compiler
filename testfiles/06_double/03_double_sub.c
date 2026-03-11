801c9b963d1c297c34c379e828b0c39d96c19675
lMarnas <leo.marnas@gmail.com>
2026-03-11 09:42:22 +0100

    feat/double: add test files from double branch & enforce C++ 20 for supporting bit cast for x86 asm gen

---
+++ testfiles/06_double/03_double_sub.c
@@ -0,0 +1,6 @@
+int main() {
+    double a = 10.1;
+    double b = 3.9;
+    double c = a - b;
+    return c;
+}
---
+++ testfiles/06_double/04_double_mul.c
@@ -0,0 +1,6 @@
+int main() {
+    double a = 4.2;
+    double b = 3.0;
+    double c = a * b;
+    return c;
+}
