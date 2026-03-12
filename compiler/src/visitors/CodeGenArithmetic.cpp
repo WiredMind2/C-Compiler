185c5cbc7afd427bd7fb1322649387a1b5b1bfff
Klhmt <78567345+Klhmt@users.noreply.github.com>
2026-03-12 14:26:52 +0100

    feat/double: add double support in CodeGenArithmetic.cpp, CodeGenComparison.cpp and CodeGenRelational.cpp

--- compiler/src/visitors/CodeGenArithmetic.cpp
+++ compiler/src/visitors/CodeGenArithmetic.cpp
@@ -1,13 +1,20 @@
 #include "CodeGenArithmetic.h"
 #include "../CodeGenVisitor.h"
+#include "../type.h"
 #include <iostream>
 
 antlrcpp::Any visitAddition(CodeGenVisitor* visitor, ifccParser::AdditionContext *ctx)
 {
     string left = std::any_cast<string>(visitor->visit(ctx->additive()));
     string right = std::any_cast<string>(visitor->visit(ctx->multiplicative()));
-    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(INT);
-    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::add, INT, {tmp, left, right});
+    Type leftType = visitor->getCFG()->getCurrentBB()->get_var_type(left);
+    Type rightType = visitor->getCFG()->getCurrentBB()->get_var_type(right);
+    if (leftType != rightType) {
+        std::cerr << "type not identical. Not supported right now" << std::endl;
+        exit(1);
+    }
+    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(leftType);
+    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::add, leftType, {tmp, left, right});
     return tmp;
 }
 
@@ -15,8 +22,13 @@ antlrcpp::Any visitSubstraction(CodeGenVisitor* visitor, ifccParser::Substractio
 {
     string left = std::any_cast<string>(visitor->visit(ctx->additive()));
     string right = std::any_cast<string>(visitor->visit(ctx->multiplicative()));
-    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(INT);
-    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::sub, INT, {tmp, left, right});
+    Type leftType = visitor->getCFG()->getCurrentBB()->get_var_type(left);
+    Type rightType = visitor->getCFG()->getCurrentBB()->get_var_type(right);
+    if (leftType != rightType) {
+        std::cerr << "type not identical. Not supported right now" << std::endl;
+        exit(1);
+    }    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(leftType);
+    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::sub, leftType, {tmp, left, right});
     return tmp;
 }
 
@@ -24,8 +36,13 @@ antlrcpp::Any visitMultiplication(CodeGenVisitor* visitor, ifccParser::Multiplic
 {
     string left = std::any_cast<string>(visitor->visit(ctx->multiplicative()));
     string right = std::any_cast<string>(visitor->visit(ctx->unary()));
-    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(INT);
-    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::mul, INT, {tmp, left, right});
+    Type leftType = visitor->getCFG()->getCurrentBB()->get_var_type(left);
+    Type rightType = visitor->getCFG()->getCurrentBB()->get_var_type(right);
+    if (leftType != rightType) {
+        std::cerr << "type not identical. Not supported right now" << std::endl;
+        exit(1);
+    }    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(leftType);
+    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::mul, leftType, {tmp, left, right});
     return tmp;
 }
 
@@ -33,8 +50,13 @@ antlrcpp::Any visitDivision(CodeGenVisitor* visitor, ifccParser::DivisionContext
 {
     string left = std::any_cast<string>(visitor->visit(ctx->multiplicative()));
     string right = std::any_cast<string>(visitor->visit(ctx->unary()));
-    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(INT);
-    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::div, INT, {tmp, left, right});
+    Type leftType = visitor->getCFG()->getCurrentBB()->get_var_type(left);
+    Type rightType = visitor->getCFG()->getCurrentBB()->get_var_type(right);
+    if (leftType != rightType) {
+        std::cerr << "type not identical. Not supported right now" << std::endl;
+        exit(1);
+    }    string tmp = visitor->getCFG()->getCurrentBB()->create_new_tempvar(leftType);
+    visitor->getCFG()->getCurrentBB()->add_IRInstr(IRInstr::div, leftType, {tmp, left, right});
     return tmp;
 }
 
