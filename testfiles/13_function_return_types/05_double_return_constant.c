aee4fb6c6c1c9a0d617736a99cb4d7c4dd281dec
cdupic <clement.dupic@insa-lyon.fr>
2026-03-29 10:58:32 +0200

    fix(char) : function return types test files

---
+++ testfiles/13_function_return_types/05_double_return_constant.c
@@ -0,0 +1,7 @@
+double getDouble() {
+    return 3.14;
+}
+int main() {
+    return getDouble();
+}
+
---
+++ testfiles/13_function_return_types/08_mixed_return_types.c
@@ -0,0 +1,12 @@
+int intFunc() {
+    return 100;
+}
+char charFunc() {
+    return 'X';
+}
+int main() {
+    int a = intFunc();
+    char b = charFunc();
+    return a + b;
+}
+
