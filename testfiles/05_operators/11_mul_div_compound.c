3ed363eb2549ef022acc44426a0c19bd2a2db231
William <william@michaud.re>
2026-04-05 10:58:49 +0200

    Refactor tests

---
+++ testfiles/05_operators/11_mul_div_compound.c
@@ -0,0 +1,7 @@
+// Test '*=' compound assignment
+int main() {
+    int a = 6;
+    int b = 7;
+    a *= b; // a = 42
+    return a;
+}
---
+++ testfiles/05_operators/12_div_compound.c
@@ -0,0 +1,7 @@
+// Test '/=' compound assignment
+int main() {
+    int a = 84;
+    int b = 2;
+    a /= b; // a = 42
+    return a;
+}
